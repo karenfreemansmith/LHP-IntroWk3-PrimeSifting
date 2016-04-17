f0ad827e7d4016847cb1aa022155472d24ac5988
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-16 19:09:44 -0700

    filter multiples of 2 from array

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,23 @@
 
 $("#findPrimes").click(function() {
   var yourPrimes=[];
+  var prime=2;
   var yourNumber=parseInt($("#primesTo").val());
   for(i=1; i<=yourNumber; i++) {
     yourPrimes.push(i);
   }
+  //for(i=1; i<=yourPrimes.length; i++) {
+    yourPrimes.forEach(function() {
+      var filtered=yourPrimes.filter(isPrime);
+      yourPrimes=filtered;
+    });
+    //alert(yourPrimes[i]);
+  //}
+
 
   $("#results").text(yourPrimes);
+
+  function isPrime(value) {
+    return value %prime !== 0 && value!== prime;
+  }
 });
