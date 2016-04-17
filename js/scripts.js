9743c25113d628c20afe1ed56080744e5620823d
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-16 18:51:34 -0700

    fill array with all integers up to and including the one you submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+
+$("#findPrimes").click(function() {
+  var yourPrimes=[];
+  var yourNumber=parseInt($("#primesTo").val());
+  for(i=1; i<=yourNumber; i++) {
+    yourPrimes.push(i);
+  }
+
+  $("#results").text(yourPrimes);
+});
